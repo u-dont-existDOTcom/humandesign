8c3bf02c3cd69f750019fa4c572f82f777381aa1
u-dont-exist.com <joel@loveyhuasca.info>
2026-08-28 17:56:41 +0000

    Export relationship questionnaire helpers

--- src/hdmatch/relationship/__init__.py
+++ src/hdmatch/relationship/__init__.py
@@ -14,6 +14,14 @@ from .analysis import (
     analyze_partnership,
     snapshot_from_chart,
 )
+from .questionnaire import (
+    RelationshipQuestion,
+    RelationshipQuestionnaireSpec,
+    load_relationship_questionnaire,
+    question_by_id,
+    select_next_capture_question,
+    select_next_validation_question,
+)
 from .uncertain_time import (
     AnalyzedPartnershipInterval,
     PartnerTimeCandidate,
@@ -30,9 +38,15 @@ __all__ = [
     "PartnerTimeCandidate",
     "PartnershipAnalysis",
     "PartnershipSnapshot",
+    "RelationshipQuestion",
+    "RelationshipQuestionnaireSpec",
     "SunEarthNodeAlignment",
     "UncertainPartnerTimeSummary",
     "analyze_partnership",
+    "load_relationship_questionnaire",
+    "question_by_id",
+    "select_next_capture_question",
+    "select_next_validation_question",
     "snapshot_from_chart",
     "summarize_uncertain_partner_time",
 ]
